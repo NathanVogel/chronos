72aa5495d0bb5fc7bc43b7929be4e05b9bfc0d3d
Nathan Vogel <nathan@nathanvogel.com>
2016-10-30 01:05:16 +0200

    Stars in background. System looks clean

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@
 // });
 
 
+print = (str) => {
+  console.log(str);
+}
+
 class Point {
   constructor(x, y, z) {
     this.x = x;
@@ -26,20 +30,24 @@ class Planet {
     this.celestialParent = celestialParent;
     this.radius = radius;
 
+    // Set a default position for the star. Which is always the center of
     this.position = new Point(0, 0, 0);
   }
 
   update() {
     push();
+    // If this object has a parent, calculate its position.
+    // Note : if it doesn't have one, it'll use the default 0, 0
     if (this.celestialParent != null) {
-      // translate(this.celestialParent.position.x, this.celestialParent.position.y);
-      // calculate current pos
+      // Calculate the position relative to the parent object.
       this.position.x = Math.sin(this.getAngle()) * this.orbitRadius;
       this.position.y = Math.cos(this.getAngle()) * this.orbitRadius;
       this.position.z = this.celestialParent.position.z;
+      // Offset our position to move to the parent object.
       this.position.x += this.celestialParent.position.x;
       this.position.y += this.celestialParent.position.y;
     }
+    // Move and draw.
     translate(this.position.x, this.position.y);
     this.drawPlanet();
     pop();
@@ -48,9 +56,12 @@ class Planet {
 }
 
 
+
+// ====== System Parts draw ======
+
 drawSun = function() {
   fill(240, 240, 0);
-  ellipse(0, 0, this.radius * 2);
+  ellipse(0, 0, this.radius * 2, this.radius * 2, 70);
 }
 
 drawEarth = function() {
@@ -88,6 +99,10 @@ draw3DMoon = function() {
 }
 
 
+// ====== CALCULATIONS ======
+
+// Calculate angles
+
 getNoAngle = () => {
   return 0;
 }
@@ -111,35 +126,114 @@ getMinutesAngle = () => {
   return a;
 }
 
+// Calculate the maximum size of the system
 
+getSystemSpan = (firstStar) => {
+  var maxSystemSpan = 0;
+  let currentPlanet = firstStar;
+  while (currentPlanet) {
+    maxSystemSpan += currentPlanet.orbitRadius;
+    currentPlanet = currentPlanet.satellite;
+  }
+  return maxSystemSpan;
+}
+
+
+
+
+class Star {
+
+  constructor(x, y, z, intensity) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.intensity = intensity;
+  }
+
+  update() {
+    strokeWeight(this.intensity);
+    stroke(255);
+    push();
+    translate(this.x, this.y, this.z);
+    ellipse(0, 0, this.intensity);
+    pop();
+  }
+}
+
+
+// ====== PREFERENCES ======
+
+var followPlanet = true;
+var systemDensity = 2; // Per 100x100 pixels surface at origin depth
+var systemSkyDistance = 300;
+
+// Create system
 var star = new Planet(drawSun, getNoAngle, 100, 0, null);
 star.satellite = new Planet(drawEarth, getSecondsAngle, 30, 300, star);
-star.satellite.satellite = new Planet(drawMoon, getMillisAngle, 10, 70, star.satellite);
+star.satellite.satellite = new Planet(drawMoon, getMillisAngle, 10, 70,
+  star.satellite
+);
 
+
+
+
+// ====== SETUP ======
+
+var stars = [];
+var currentScale = 2;
+var maxSystemSpan = getSystemSpan(star);
+var systemSizeX = maxSystemSpan * 5 * systemSkyDistance / 300;
+var systemSizeY = systemSizeX;
+var systemStarCount = (systemSizeX * systemSizeY / (100 * 100)) * systemDensity;
+print("Star count : " + systemStarCount);
+if (systemStarCount > 1000) {
+  systemStarCount = 1000;
+}
 
 setup = () => {
   createCanvas(window.innerWidth, window.innerHeight, WEBGL);
+  for (let i = 0; i < systemStarCount; i++) {
+    stars.push(new Star(
+      Math.random() * systemSizeX - systemSizeX / 2,
+      Math.random() * systemSizeY - systemSizeY / 2,
+      Math.random() * -systemSkyDistance * systemDensity - systemSkyDistance,
+      Math.random() * 3)
+    );
+  }
 
-  var followPlanet = true;
 }
 
+
+// ====== DRAW ======
+// Draw every frame
 draw = () => {
-  //rotateX(frameCount * 0.003);
-  //rotateY(frameCount * 0.004);
+  // Some 3D stuff
+  // rotateX(frameCount * 0.001);
+  // rotateY(frameCount * 0.0007);
+  // var dirY = (mouseY / height - 0.5) * 2;
+  // var dirX = (mouseX / width - 0.5) * 2;
+  // pointLight(255, 230, 230, 0.1, 0, 0, 0);
+  //mouseX - width / 2, -mouseY + height / 2, 500);
 
   background(20);
+
+  // Move our camera
   if (followPlanet) {
-    scale(2);
+    scale(currentScale);
     translate(-star.satellite.position.x, -star.satellite.position.y);
   }
 
-  var dirY = (mouseY / height - 0.5) * 2;
-  var dirX = (mouseX / width - 0.5) * 2;
-  pointLight(255, 230, 230, 0.1, 0, 0, 0); //mouseX - width / 2, -mouseY + height / 2, 500);
-
+  // Draw the planets
   let currentPlanet = star;
   while (currentPlanet) {
     currentPlanet.update();
-    currentPlanet = currentPlanet.satellite
+    currentPlanet = currentPlanet.satellite;
   }
+
+  // Draw the sky
+  for (let i = 0; i < stars.length; i++) {
+    stars[i].update();
+  }
+
+
 }
